46e3ef078e8757fb4be50cc621973e962a1e753e
Zhiyao Shu <yaoshu0326@berkeley.edu>
2023-07-10 15:37:19 -0700

    upload

--- node-http/server.js
+++ node-http/server.js
@@ -1,20 +1,22 @@
 const express = require("express");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
+// const session = require("express-session");
+// const cookieParser = require("cookie-parser");
 const MongoClient = require("mongodb");
 
+const authRoutes = require("./routers/authRouter");
+const { Post, User } = require("./model-user");
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(
-  session({
-    secret: "your-secret-key",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+// app.use(cookieParser());
+// app.use(
+//   session({
+//     secret: "your-secret-key",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
 const client = new MongoClient("mongodb://0.0.0.0:27017/nodemongo");
 client.connect(),
