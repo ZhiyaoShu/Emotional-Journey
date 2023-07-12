9cf59e3425f7bbbb4d58042a222dd80bd54b0ab9
Zhiyao Shu <yaoshu0326@berkeley.edu>
2023-07-11 21:11:18 -0700

    set up fundation

--- public/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import App from "../src/component/App.vue";
-import router from "../src/router-vue";
-
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./src/authRouter");
 const { Post, User } = require("./src/model-user");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use((req, res, next) => {
+//   res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+// });
 
 mongoose
   .connect("mongodb://0.0.0.0:27017/nodemongo", {
