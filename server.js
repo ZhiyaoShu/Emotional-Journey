07b9527be8432f6abab3ffbc7f6487bbcbd459e6
Zhiyao Shu <yaoshu0326@berkeley.edu>
2023-07-13 14:59:20 -0700

    revise css

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./src/authRouter");
 const { Post, User } = require("./src/model-user");
 const path = require("path");
+// const { fileURLToPath } = require("url");
 
 const app = express();
 
@@ -21,12 +22,20 @@ mongoose
     console.log("MongoDB connection error: ", err);
   });
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.static(path.join(__dirname, "public", "index.html"), {
+    setHeaders: (res, filePath) => {
+      if (filePath.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      }
+    },
+  })
+);
 
 app.use("/", authRoutes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public"));
 });
 
 app.listen(3000, () => {
